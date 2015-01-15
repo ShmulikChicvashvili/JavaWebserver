103ec4637c1934a6bf9d32b84976a37b1b2fb678
chicvaXT <shmulikjkech@gmail.com>
2015-01-15 19:54:24 +0200

    e

--- src/il/technion/cs236369/webserver/simplefilter/FilterChainImpl.java
+++ src/il/technion/cs236369/webserver/simplefilter/FilterChainImpl.java
@@ -1,23 +1,34 @@
+
 package il.technion.cs236369.webserver.simplefilter;
+
 
 import java.util.List;
 
 import org.apache.http.HttpRequest;
 import org.apache.http.HttpResponse;
 
-public class FilterChainImpl implements FilterChain {
 
-	public FilterChainImpl(List<SimpleFilterWrapper> filters) {
+
+
+public class FilterChainImpl implements FilterChain
+{
+	
+	public FilterChainImpl(List<SimpleFilterWrapper> filters)
+	{
 		this.filters = filters;
 		index = 0;
 	}
-
+	
+	
 	@Override
-	public void doFilter(HttpRequest request, HttpResponse response) {
-		// TODO Auto-generated method stub
-
+	public void doFilter(HttpRequest request, HttpResponse response)
+	{
+		// SimpleFilter filter = filters.get(index);
 	}
-
-	private List<SimpleFilterWrapper> filters;
-	private int index;
+	
+	
+	
+	private final List<SimpleFilterWrapper> filters;
+	
+	private final int index;
 }
